53c59454e332c0360a2ecad1f2b6077e9ba70591
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-28 17:23:56 -0800

    added second route by accessing url id

--- express_server.js
+++ express_server.js
@@ -4,10 +4,16 @@ const PORT = 8080; // default port 8080
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 app.set("view engine", "ejs");
+
+app.get('/urls/:id', (req, res) => {
+  console.log(req.params);
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render('urls_show', templateVars);
+});
 
 app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
