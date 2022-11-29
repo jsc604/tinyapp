ffbbbde0d1c44762d5731de102f5ddb568e77a23
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-28 18:39:28 -0800

    added more get routes and post route and created new page for users to input data

--- express_server.js
+++ express_server.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8089; // default port 8080
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
+function generateRandomString() {
+  return Math.random().toString(36).slice(2, 8);
+}
+
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get('/urls/:id', (req, res) => {
-  console.log(req.params);
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render('urls_show', templateVars);
 });
